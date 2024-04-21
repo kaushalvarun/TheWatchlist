e94a0f5c30e86f49c20acf997a2bee22eb8f334d
KaushalCorp <kaushalvarunkv@gmail.com>
2024-04-22 01:17:45 +0530

    Deployment ready final

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/TheWatchlist/'
-  }
-  return config
+export default defineConfig({
+  plugins: [react()],
 })
