8ff7687a27d03291813caab24f9e5d18bbb19648
KaushalCorp <kaushalvarunkv@gmail.com>
2024-04-21 23:56:57 +0530

    Making deploy ready

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // to be compatible wit github pages
   base: "/TheWatchlist/",
   plugins: [react()],
 })
